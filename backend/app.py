633dcfd3f9ec62a31a69ececf92fa28308164f99
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-09-22 05:15:55 -0400

    removed cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from routes import init_routes  # Import your routes
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/takeout": {"origins": "https://matchtube.xyz/"}})
 
 # Initialize routes
 init_routes(app)
