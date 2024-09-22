56c11b4af404555cb3a8cccebcf8fd8008a818de
Sophia E <125570727+evanss2025@users.noreply.github.com>
2024-09-21 23:19:54 -0400

    account/login ui update

--- frontend/src/TakeoutUpload.jsx
+++ frontend/src/TakeoutUpload.jsx
@@ -36,7 +36,6 @@ const TakeoutUpload = () => {
 
     return (
         <div className='flex-col min-h-screen w-full bg-cover bg-gradient-to-r from-tinderPink via-tinderRed to-tinderOrange flex justify-center items-center'> 
-            <PropelAuth />
             <img 
                 src="logo2.png" 
                 className='w-3/4 md:w-1/3 mb-8'
@@ -49,7 +48,7 @@ const TakeoutUpload = () => {
                         <input 
                         type="file" 
                         onChange={onFileChange}
-                        className='flex flex-col file:transition file:ease-in-out file:hover:-translate-y-1 file:duration-300 file:border-none file:bg-gradient-to-r from-tinderPink via-tinderRed to-tinderOrange file:p-2 file:px-8 file:text-white md:text-xl file:mt-3 file:rounded-xl'/>
+                        className='flex flex-col file:cursor-pointer file:transition file:ease-in-out file:hover:-translate-y-1 file:duration-300 file:border-none file:bg-gradient-to-r from-tinderPink via-tinderRed to-tinderOrange file:p-2 file:px-8 file:text-white md:text-xl file:mt-3 file:rounded-xl'/>
                         <button 
                             className="w-1/2 md:w-full transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 bg-gradient-to-r mt-5 rounded-xl from-tinderPink via-tinderRed to-tinderOrange p-2 px-8 text-white md:text-xl"
                             type="submit"
@@ -58,6 +57,9 @@ const TakeoutUpload = () => {
                     </form>
                 </div>
                 {responseMessage && <p>{responseMessage}</p>}
+            </div>
+            <div id='account' className='bg-white rounded-2xl w-4/5 md:w-1/2 p-8 flex flex-col items-center justify-center mt-5'>
+                <PropelAuth />
             </div>
         </div>
     );
--- frontend/src/components/PropelAuth.jsx
+++ frontend/src/components/PropelAuth.jsx
@@ -13,18 +13,26 @@ const PropelAuth = withAuthInfo((props) => {
 
     if (props.isLoggedIn) {
         return (
-            <div>
-                <p>You are logged in as {props.user.email}</p>
-                <button onClick={() => redirectToAccountPage()}>Account</button>
-                <button onClick={() => logoutFunction(true)}>Logout</button>
+            <div className="mt-5">
+                <p className="text-xl">You are logged in as {props.user.email}</p>
+                <button 
+                    className="mt-5 mx-2 w-1/2 md:w-full transition ease-in-out hover:-translate-y-1 duration-300 border-4 border-tinderOrange p-2 px-8 md:text-lg rounded-xl"
+                    onClick={() => redirectToAccountPage()}>Account</button>
+                <button 
+                    className="mt-5 mx-2 w-1/2 md:w-full transition ease-in-out hover:-translate-y-1 duration-300 border-4 border-tinderOrange p-2 px-8 md:text-lg rounded-xl"
+                    onClick={() => logoutFunction(true)}>Logout</button>
             </div>
         );
     } else {
         return (
             <div>
                 <p>You are not logged in</p>
-                <button onClick={() => redirectToLoginPage()}>Login</button>
-                <button onClick={() => redirectToSignupPage()}>Signup</button>
+                <button 
+                    className="mt-5 mx-5 w-1/2 md:w-full transition ease-in-out hover:-translate-y-1 duration-300 border-4 border-tinderOrange p-2 px-8 md:text-lg rounded-xl"
+                    onClick={() => redirectToLoginPage()}>Login</button>
+                <button
+                    className="mt-5 mx-5 w-1/2 md:w-full transition ease-in-out hover:-translate-y-1 duration-300 border-4 border-tinderOrange p-2 px-8 md:text-lg rounded-xl"
+                    onClick={() => redirectToSignupPage()}>Signup</button>
             </div>
         );
     }
