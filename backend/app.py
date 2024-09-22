4497ad3d64be0c65ec947274a8166da2586973dd
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-09-22 04:33:32 -0400

    set port to env var

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,5 @@ CORS(app)
 init_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host='0.0.0.0', port=port)
